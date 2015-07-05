5293718acf94760b76b3203daba01b23c1293887
Andrew Naish <anaish@lycos.com>
2015-07-05 15:33:51 +1000

    up to firing boss bullet

--- core/src/com/nrg/kelly/stages/actors/BossActor.java
+++ core/src/com/nrg/kelly/stages/actors/BossActor.java
@@ -2,11 +2,19 @@ package com.nrg.kelly.stages.actors;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Timer;
+import com.google.common.base.Optional;
 import com.nrg.kelly.config.CameraConfig;
 import com.nrg.kelly.config.actors.ActorConfig;
 import com.nrg.kelly.config.actors.Enemy;
 
+import java.util.Set;
+
+
 public class BossActor extends EnemyActor{
+
+    private boolean isInFiringPosition = false;
+    private Optional<Timer.Task> fireSchedule = Optional.absent();
 
     public BossActor(Enemy enemy, CameraConfig cameraConfig) {
         super(enemy, cameraConfig);
@@ -20,11 +28,37 @@ public class BossActor extends EnemyActor{
             final float pos = this.getTextureBounds().getX() + this.getTextureBounds().getWidth();
             if(pos < Gdx.graphics.getWidth()){
                 body.setLinearVelocity(0f, 0f);
-
+                this.isInFiringPosition = true;
             } else {
                 body.setLinearVelocity(linearVelocity);
             }
+            if(canFireBullet()){
+                fireBullet();
+            }
+
         }
     }
+
+    private boolean canFireBullet() {
+        boolean canFire = isInFiringPosition;
+        for(Timer.Task task : fireSchedule.asSet()) {
+            if(task.isScheduled()){
+                canFire = false;
+            }
+        }
+        return canFire;
+    }
+
+    private void fireBullet() {
+
+        fireSchedule = Optional.of(Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                Gdx.app.log(this.getClass().getName(), "Fired bullet.");
+            }
+        }, 1.2f));
+
+    }
+
 
 }
